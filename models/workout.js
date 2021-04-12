630bd45aa340800af092709eadb88e645eb5ce62
Tianwes <willnotsuffice@gmail.com>
2021-04-12 12:07:07 -0400

    Model Workout.js- wrote code, haven't tested

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,45 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
-    type: Date,
-    default: Date.now
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name",
+        },
+        weight: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+        duration: {
+            type: Number,
+            required: "Enter the duration of exercise (minutes)",
+          },
+      },
+    ],
   }
-});
+);
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Workout = mongoose.model("workout", workoutSchema);
 
-module.exports = Transaction;
+module.exports = Workout;
